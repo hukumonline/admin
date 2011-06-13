1266792ca6b4fd42ecdafd9db7e236741e2e372f
root <root@localhost.localdomain>
2011-06-13 11:44:34 +0700

    change group user for Dc Admin migration

--- app/modules/customer/controllers/MigrationController.php
+++ app/modules/customer/controllers/MigrationController.php
@@ -39,7 +39,7 @@ class Customer_MigrationController extends Zend_Controller_Action
 
         echo $title.'<br>';
 
-        $groupId = 36;
+        $groupId = 20;
 
         require_once(CONFIG_PATH.'/master-status.php');
 
