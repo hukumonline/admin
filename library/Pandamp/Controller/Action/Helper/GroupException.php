44dcc4e089a10aed0366f6a50c95375edccea4ab
root <root@localhost.localdomain>
2011-06-13 13:33:12 +0700

    modifid group exception 2nd times

--- library/Pandamp/Controller/Action/Helper/GroupException.php
+++ library/Pandamp/Controller/Action/Helper/GroupException.php
@@ -15,6 +15,6 @@ class Pandamp_Controller_Action_Helper_GroupException
             $aReturn[$i] = $rowset[$i]['value'];
         }
         
-        return ($aReturn)? $aReturn : '';
+        return (isset($aReturn))? $aReturn : '';
     }
 }
