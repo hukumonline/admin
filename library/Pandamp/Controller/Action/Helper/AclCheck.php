a216fcc7eef429096b814e96ecf2f1388a11b345
root <root@localhost.localdomain>
2011-06-21 17:58:23 +0700

    modified header viewer

--- library/Pandamp/Controller/Action/Helper/AclCheck.php
+++ library/Pandamp/Controller/Action/Helper/AclCheck.php
@@ -7,7 +7,7 @@
  */
 class Pandamp_Controller_Action_Helper_AclCheck
 {
-    public function aclCheck($section, $aco)
+    public function aclCheck($section, $aco, $axoSectionValue=false, $axoValue=false)
     {
         $auth = Zend_Auth::getInstance();
         if (!$auth->hasIdentity()) {
@@ -17,6 +17,6 @@ class Pandamp_Controller_Action_Helper_AclCheck
         $username = $auth->getIdentity()->username;
         
 		$acl = Pandamp_Acl::manager();
-		return $acl->checkAcl($section,$aco,'user', $username, false,false);
+		return $acl->checkAcl($section,$aco,'user', $username, $axoSectionValue,$axoValue);
     }
 }
