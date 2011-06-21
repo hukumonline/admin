6be06f5c216242e19d116d1d5409a64c3bdaab60
root <root@localhost.localdomain>
2011-06-21 17:47:30 +0700

    modified list of users

--- app/modules/customer/controllers/UserController.php
+++ app/modules/customer/controllers/UserController.php
@@ -107,6 +107,11 @@ class Customer_UserController extends Zend_Controller_Action
     }
     function listAction()
     {
+        if (!Pandamp_Controller_Action_Helper_IsAllowed::isAllowed('membership','all'))
+        {
+            $this->_redirect(ROOT_URL.'/'.$this->_zl->getLanguage().'/error/restricted');
+        }
+        
         $this->_helper->layout->setLayout('layout-customer-credential');
 
         $userList = App_Model_Show_User::show()->getUserList();
