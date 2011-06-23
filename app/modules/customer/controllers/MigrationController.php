b4d02b8117a439eb835cdd77e6345d6409d2d074
root <root@localhost.localdomain>
2011-06-23 20:10:20 +0700

    modified PackageId 5th times

--- app/modules/customer/controllers/MigrationController.php
+++ app/modules/customer/controllers/MigrationController.php
@@ -86,7 +86,7 @@ class Customer_MigrationController extends Zend_Controller_Action
 				$modelUser = new App_Model_Db_Table_User();
 				$rowUser = $modelUser->fetchRow("username='".$rowUser['username']."'");
 					$groupName = $this->getGroupName($rowUser['packageId']);
-					echo $rowUser['packageId'];die;
+					echo $data['packageId'];die;
 				if (!$rowUser) 
 				{
 					$result = $modelUser->insert($data);
