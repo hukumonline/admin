3ab0f99a9ba6f7c433655bf3ce72dfb967224923
nihki <nihki@hukumonline.com>
2011-06-24 15:37:14 +0700

    modified packageId

--- app/modules/customer/controllers/MigrationController.php
+++ app/modules/customer/controllers/MigrationController.php
@@ -44,7 +44,7 @@ class Customer_MigrationController extends Zend_Controller_Action
 
         echo $title.'<br>';
 
-        $groupId = 26;
+        $groupId = 27;
 
         require_once(CONFIG_PATH.'/master-status.php');
 
