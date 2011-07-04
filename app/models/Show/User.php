91b854b5ad7cb49393f3606fcf778d83fe527613
nihki <nihki@hukumonline.com>
2011-07-04 16:38:35 +0700

    debug users

--- app/models/Show/User.php
+++ app/models/Show/User.php
@@ -11,11 +11,16 @@ class App_Model_Show_User extends App_Model_Db_DefaultAdapter
      * class instance object
      */
     private static $_instance;
+    
+    private static $_db;
 
     /**
      * de-activate constructor
      */
-    final private function  __construct() {}
+    final private function  __construct() 
+    {
+    	self::$_db = Zend_Registry::get('db2');
+    }
 
      /**
       * de-activate object cloning
@@ -47,9 +52,11 @@ class App_Model_Show_User extends App_Model_Db_DefaultAdapter
     public function getUserList()
     {
         $db = parent::_dbSelect();
-        $select = $db->from('KutuUser','*','hid');
+        $select = $db->from('KutuUser');
+        
+        $conn = self::$_db;
 
-        $result = parent::_getDefaultAdapter()->fetchAll($select);
+        $result = $conn->fetchAll($select);
 
         return $result;
     }
--- app/modules/api/controllers/UserController.php
+++ app/modules/api/controllers/UserController.php
@@ -158,7 +158,7 @@ class Api_UserController extends Zend_Controller_Action
 		$tblUser = new App_Model_Db_Table_User();
 		//echo $q;die();
 		$rowset = $tblUser->fetchAll($sWhere, 'kopel ASC', $limit, $start);
-		echo count($rowset);die();
+		
 		if(count($rowset)==0)
 		{
 			$a['users'][0]['kopel']= 'XXX';
