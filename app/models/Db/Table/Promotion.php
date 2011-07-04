28981bc29203f2cc979fba62e41acee5667e83f7
nihki <nihki@hukumonline.com>
2011-07-04 18:15:03 +0700

    modified promotion table

--- app/models/Db/Table/Promotion.php
+++ app/models/Db/Table/Promotion.php
@@ -3,4 +3,10 @@ class App_Model_Db_Table_Promotion extends Zend_Db_Table_Abstract
 {
 	protected $_name = 'KutuUserPromotion';
 	protected $_schema = 'hid';
+    protected function  _setupDatabaseAdapter()
+    {
+        $this->_db = Zend_Registry::get('db2');
+
+        parent::_setupDatabaseAdapter();
+    }
 }
