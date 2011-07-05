5e3e33c364256a99781122d9425952494a0e46d6
nihki <nihki@hukumonline.com>
2011-07-05 13:05:29 +0700

    modified acl controller in dev module

--- app/modules/dev/controllers/AclController.php
+++ app/modules/dev/controllers/AclController.php
@@ -12,18 +12,23 @@ class Dev_AclController extends Zend_Controller_Action
         $this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender(TRUE);
 
-        $acl = Pandamp_Acl::manager();
-        if ($acl->isAllowed('dedi','dms','create'))
-            echo 'ALLOWED';
-        else
-            echo 'NO ACCESS';
+//        $acl = Pandamp_Acl::manager();
+//        if ($acl->isAllowed('dedi','dms','create'))
+//            echo 'ALLOWED';
+//        else
+//            echo 'NO ACCESS';
             
 
-//		$aReturn = $acl->getUserGroupIds('pengguna1'); 
+//		$aReturn = $acl->getUserGroupIds('zapatista'); 
 //		if ($acl->getPermissionsOnContent('', $aReturn[1], 'membership'))
 //			echo 'ALLOWED';
 //		else 
 //			echo 'NO ACCESS';
+
+		if ($acl->checkAcl('action', 'read', 'user', 'zapatista', 'content', 'all-access'))
+			echo 'ALLOWED';
+		else 
+			echo 'NO ACCESS';
 		
 		
     }
