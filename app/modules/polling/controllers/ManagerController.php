4b5bfceca7c4707efe0a256b404f33f8ce1c0617
nihki <nihki@hukumonline.com>
2011-07-11 11:55:00 +0700

    modified module polling

--- app/modules/polling/controllers/ManagerController.php
+++ app/modules/polling/controllers/ManagerController.php
@@ -9,13 +9,16 @@ class Polling_ManagerController extends Zend_Controller_Action
 {
     function viewAction()
     {
-        $time = time();
-        $date = date("Y-m-d H:i:s", $time);
-
-        $rowPoll = App_Model_Show_Poll::show()->getPollByDate($date);
-        $this->view->rowPoll = $rowPoll;
-
-        $rowOpt = App_Model_Show_PollOption::show()->getPollOption($rowPoll['guid']);
-        $this->view->rowOpt = $rowOpt;
+		$tblPolling = new App_Model_Db_Table_Poll();
+		$tblOption = new App_Model_Db_Table_PollOption();
+		
+		$time = time();
+		$date = date("Y-m-d H:i:s", $time);
+		
+		$rowPoll = $tblPolling->fetchRow("checkedTime < '$date'","checkedTime DESC");
+		$this->view->rowPoll = $rowPoll;
+		
+		$rowOpt = $tblOption->fetchAll("pollGuid='$rowPoll->guid'","text ASC");
+		$this->view->rowOpt = $rowOpt;
     }
 }
