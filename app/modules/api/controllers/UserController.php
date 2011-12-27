e8ea9efa8d0b619b620f1b9e990f3d58f8792d4a
nihki <nihki@hukumonline.com>
2011-12-27 16:23:11 +0700

    modified user module -> show password field for master group

--- app/modules/api/controllers/UserController.php
+++ app/modules/api/controllers/UserController.php
@@ -205,7 +205,7 @@ class Api_UserController extends Zend_Controller_Action
 						if (Pandamp_Controller_Action_Helper_UserGroup::userGroup($this->_user->packageId) == "Master") {
 
 							$password = $obj->decryptPassword($row->password);
-							$password = ($password)? $password : "";	
+							$password = ($password !== "null")? $password : "";	
 							
 							$passwd .= "<tr><td>&nbsp;</td><td colspan='6' style='color:green;'>password:<a href='".ROOT_URL.'/'.$this->_zl->getLanguage().'/customer/user/edit/id/'.$row->kopel."'>".$password."</a></td></tr>";
 							
