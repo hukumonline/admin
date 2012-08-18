7ccf28cc949a81b6e42454ac38545530d2e9b643
nihki <nihki@hukumonline.com>
2012-08-18 14:38:23 +0700

    modify dev search controller

--- app/modules/dev/controllers/SearchController.php
+++ app/modules/dev/controllers/SearchController.php
@@ -108,7 +108,7 @@ class Dev_SearchController extends Zend_Controller_Action
     	//$query="SELECT * FROM KutuCatalog WHERE profileGuid IN ('ild','ile')";
     	//$query="SELECT * FROM KutuCatalog WHERE profileGuid IN ('partner','klinik','kategoriklinik','author')";
     	//$query="SELECT * FROM KutuCatalog WHERE profileGuid IN ('hot_issue_ile','ilb_english_rules','ild_english_rules','manufacturing_&_industry')";
-    	$query="SELECT * FROM KutuCatalog WHERE profileGuid = 'news' limit 30,30";
+    	$query="SELECT * FROM KutuCatalog WHERE profileGuid = 'news' limit 60,30";
     	//$query="SELECT * FROM KutuCatalog WHERE profileGuid IN ('oil_and_gas','telecommunications_and_media')";
     	
     	$results = $db->query($query);
