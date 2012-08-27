8b8e4ef0c20f20e24e94594175b5c2ba4cae2d6f
unknown <kiky@.(none)>
2012-08-27 16:05:21 +0700

    modify default index

--- app/modules/default/controllers/IndexController.php
+++ app/modules/default/controllers/IndexController.php
@@ -8,17 +8,15 @@ class IndexController extends Zend_Controller_Action
 	}
 	public function __call($method, $args)
 	{
-		/*
-            $tw = Zend_Registry::get('twurfl');
-            if(!$tw->getDeviceCapability("is_wireless_device")){
-                $this->_forward('index','index','admin');
-            }
-            else
-            {
-            	echo "MOBILE";
-                //$this->_forward('index','index','msite');
-            }
-		*/
+        $tw = Zend_Registry::get('twurfl');
+        if(!$tw->getDeviceCapability("is_wireless_device")){
+            $this->_forward('index','index','admin');
+        }
+        else
+        {
+        	echo "MOBILE";
+            //$this->_forward('index','index','msite');
+        }
 	}
 }
 
