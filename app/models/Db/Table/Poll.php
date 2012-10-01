407d8c890d53b1b3bb465049828fc3effa370ee5
nihki <nihki@hukumonline.com>
2012-10-01 14:13:54 +0700

    modify model table poll

--- app/models/Db/Table/Poll.php
+++ app/models/Db/Table/Poll.php
@@ -9,4 +9,16 @@ class App_Model_Db_Table_Poll extends Zend_Db_Table_Abstract
 {
     protected $_name = 'polls';
     protected $_rowClass = 'App_Model_Db_Table_Row_Poll';
+    
+	public function insert (array $data)
+	{
+		if (empty($data['guid']))
+		{
+			$guidMan = new Pandamp_Core_Guid;
+			$data['guid'] = $guidMan->generateGuid();
+		}
+		
+		return parent::insert($data);
+		
+	}
 }
