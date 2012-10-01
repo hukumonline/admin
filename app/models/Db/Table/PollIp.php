82a40c29fa0d53ea543ace6b8dd67975fcaebcfd
nihki <nihki@hukumonline.com>
2012-10-01 14:18:14 +0700

    modify model table pollIp and pollOption

--- app/models/Db/Table/PollIp.php
+++ app/models/Db/Table/PollIp.php
@@ -8,4 +8,16 @@
 class App_Model_Db_Table_PollIp extends Zend_Db_Table_Abstract
 {
     protected $_name = 'pollsIp';
+    
+	public function insert (array $data)
+	{
+		if (empty($data['guid']))
+		{
+			$guidMan = new Pandamp_Core_Guid;
+			$data['guid'] = $guidMan->generateGuid();
+		}
+		
+		return parent::insert($data);
+		
+	}
 }
--- app/models/Db/Table/PollOption.php
+++ app/models/Db/Table/PollOption.php
@@ -8,4 +8,16 @@
 class App_Model_Db_Table_PollOption extends Zend_Db_Table_Abstract
 {
     protected $_name = 'options';
+    
+	public function insert (array $data)
+	{
+		if (empty($data['guid']))
+		{
+			$guidMan = new Pandamp_Core_Guid;
+			$data['guid'] = $guidMan->generateGuid();
+		}
+		
+		return parent::insert($data);
+		
+	}
 }
