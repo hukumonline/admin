43485698065525accf72ea581a06798cfa5c3884
nihki <nihki@hukumonline.com>
2012-11-02 14:01:37 +0700

    debug dev aclcontroller

--- app/modules/dev/controllers/AclController.php
+++ app/modules/dev/controllers/AclController.php
@@ -14,7 +14,7 @@ class Dev_AclController extends Zend_Controller_Action
 
         $acl = Pandamp_Acl::manager();
         
-        if ($acl->isAllowed('nurul-cs2','dms','aclist'))
+        if ($acl->isAllowed('seyimut','dms','aclist'))
             echo 'ALLOWED';
         else
             echo 'NO ACCESS';
