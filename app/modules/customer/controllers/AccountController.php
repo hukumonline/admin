2cfcff8e647b48dcff0c352a14d29448de719642
nihki <nihki@hukumonline.com>
2012-12-21 10:05:27 +0700

    modify account controller trialInDays

--- app/modules/customer/controllers/AccountController.php
+++ app/modules/customer/controllers/AccountController.php
@@ -161,7 +161,7 @@ class Customer_AccountController extends Zend_Controller_Action
 			,'educationId'		=> ($value['education'])? $value['education'] : 0
 			,'expenseId'		=> ($value['expense'])? $value['expense'] : 0
 			,'paymentId'		=> ($value['payment'])? $value['payment'] : 0
-			,'trialInDays'		=> $value['trial']
+			,'trialInDays'		=> ($value['trial'])? $value['trial'] : 0
 			,'businessTypeId'	=> ($value['businessType'])? $value['businessType'] : 0
 			,'periodeId'		=> 1
 			,'createdDate'		=> date('Y-m-d H:i:s')
