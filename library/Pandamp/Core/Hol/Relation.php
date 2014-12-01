8210b52bd4ff58eb55d29e35371c2353dab98bb3
Nihki Prihadi <nihki@hukumonline.com>
2014-12-01 13:38:20 +0700

    modify core getchild relation

--- library/Pandamp/Core/Hol/Relation.php
+++ library/Pandamp/Core/Hol/Relation.php
@@ -32,7 +32,7 @@ class Pandamp_Core_Hol_Relation
     	}
     		 
     	$where = "relatedGuid='$guid' AND relateAs IN ('REPEAL','AMEND','ESTABLISH')";
-    	$rowsetRelatedItem = $tblRelatedItem->fetchAll($where,'itemGuid DESC');
+    	$rowsetRelatedItem = $tblRelatedItem->fetchAll($where);
     	if (count($rowsetRelatedItem) == 0) {
     		$where = "itemGuid='$guid' AND relateAs IN ('REPEAL','AMEND','ISROOT')";
     		$rowsetRelatedItem = $tblRelatedItem->fetchRow($where);
@@ -51,7 +51,7 @@ class Pandamp_Core_Hol_Relation
     			 
     			$guid = $rowsetRelatedItem->valueStringRelation;
     			$where = "relatedGuid='$guid' AND relateAs IN ('REPEAL','AMEND')";
-    			$rowsetRelatedItem = $tblRelatedItem->fetchAll($where,'itemGuid DESC');
+    			$rowsetRelatedItem = $tblRelatedItem->fetchAll($where);
     			$newh .= App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($guid,'fixedTitle')." ".$s."<br>";
     		}
     		
@@ -75,7 +75,7 @@ class Pandamp_Core_Hol_Relation
     			$newh .= "<a href='".ROOT_URL.DS.'id'.DS.'dms/catalog/detail/guid/'.$rowsetRelatedItem1->valueStringRelation.'/node/'.$this->getNode($rowsetRelatedItem1->valueStringRelation)."'>".App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($rowsetRelatedItem1->valueStringRelation,'fixedTitle')."</a>&nbsp<a href='javascript:;' class='historynew' data-guid='$rowsetRelatedItem1->itemGuid' data-historyid='$rowsetRelatedItem1->relatedGuid' data-status='$rowsetRelatedItem1->relateAs'>Delete</a><br>";
     			$guid = $rowsetRelatedItem1->valueStringRelation;
     			$where = "relatedGuid='$guid' AND relateAs IN ('REPEAL','AMEND')";
-    			$rowsetRelatedItem = $tblRelatedItem->fetchAll($where,'itemGuid DESC');
+    			$rowsetRelatedItem = $tblRelatedItem->fetchAll($where);
     			 
     		}
     		else
