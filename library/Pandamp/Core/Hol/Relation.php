913ebd0c25c2228f604c1666b33f8b5e646b38db
Nihki Prihadi <nihki@hukumonline.com>
2015-02-18 18:05:37 +0700

    modified getHistorynew

--- library/Pandamp/Core/Hol/Relation.php
+++ library/Pandamp/Core/Hol/Relation.php
@@ -36,6 +36,12 @@ class Pandamp_Core_Hol_Relation
     				$newh .= "<a href='".ROOT_URL.DS.'id'.DS.'dms/catalog/detail/guid/'.$row1_in1->itemGuid.'/node/'.$this->getNode($row1_in1->itemGuid)."'>".App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($guidRoot,'fixedTitle')
     				."</a>&nbsp[Mencabut Sebagian]&nbsp<a href='javascript:;' class='historynew' data-guid='$row1_in1->relatedGuid' data-historyid='$row1_in1->itemGuid' data-status='$row1_in1->relateAs'>Delete</a><br>";
     			}
+    			else if (isset($row1_in1) && $row1_in1->relateAs !== 'ISROOT')
+    			{
+    				$guidRoot = $row1_in1->relatedGuid;
+    				$newh .= "<a href='".ROOT_URL.DS.'id'.DS.'dms/catalog/detail/guid/'.$row1_in1->itemGuid.'/node/'.$this->getNode($row1_in1->itemGuid)."'>".App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($guidRoot,'fixedTitle')
+    				."</a>&nbsp<a href='javascript:;' class='historynew' data-guid='$row1_in1->relatedGuid' data-historyid='$row1_in1->itemGuid' data-status='$row1_in1->relateAs'>Delete</a><br>";
+    			}
     			
     		}
     		
@@ -81,6 +87,11 @@ class Pandamp_Core_Hol_Relation
 	   					$newh .= "<a href='".ROOT_URL.DS.'id'.DS.'dms/catalog/detail/guid/'.$row2_in1->itemGuid.'/node/'.$this->getNode($row2_in1->itemGuid)."'>$title</a> $status&nbsp<a href='javascript:;' class='historynew' data-guid='$row2_in1->relatedGuid' data-historyid='$row2_in1->itemGuid' data-status='$row2_in1->relateAs'>Delete</a><br>";
 		    				
 	    				//$newh .= $this->getchild($row2_in1->itemGuid);
+    				}
+    				
+    				
+    				if ($row2_in1->relateAs === "AMEND") {
+    					$newh .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$newh;
     				}
     				
     			}
