54207bf10fddfcfbe7d009a87f846e39e12555b9
Nihki Prihadi <nihki@hukumonline.com>
2015-02-18 23:47:33 +0700

    modified getHistorynew

--- library/Pandamp/Core/Hol/Relation.php
+++ library/Pandamp/Core/Hol/Relation.php
@@ -30,18 +30,13 @@ class Pandamp_Core_Hol_Relation
     	{
     		if (isset($row1) && isset($row1->valueStringRelation))
     		{
-    			$row1_in1 = $tblRelatedItem->fetchRow("valueStringRelation='$row1->valueStringRelation'");
-    			if (isset($row1_in1) && $row1_in1->relateAs == 'ISROOT') {
+    			$row1_in1 = $tblRelatedItem->fetchRow("valueStringRelation='$row1->valueStringRelation' AND relateAs='ISROOT'");
+    			if ($row1_in1) {
     				$guidRoot = $row1_in1->relatedGuid;
     				$newh .= "<a href='".ROOT_URL.DS.'id'.DS.'dms/catalog/detail/guid/'.$row1_in1->itemGuid.'/node/'.$this->getNode($row1_in1->itemGuid)."'>".App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($guidRoot,'fixedTitle')
     				."</a>&nbsp[Mencabut Sebagian]&nbsp<a href='javascript:;' class='historynew' data-guid='$row1_in1->relatedGuid' data-historyid='$row1_in1->itemGuid' data-status='$row1_in1->relateAs'>Delete</a><br>";
     			}
-    			else if (isset($row1_in1) && $row1_in1->relateAs !== 'ISROOT')
-    			{
-    				$guidRoot = $row1_in1->relatedGuid;
-    				$newh .= "<a href='".ROOT_URL.DS.'id'.DS.'dms/catalog/detail/guid/'.$row1_in1->itemGuid.'/node/'.$this->getNode($row1_in1->itemGuid)."'>".App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($guidRoot,'fixedTitle')
-    				."</a>&nbsp<a href='javascript:;' class='historynew' data-guid='$row1_in1->relatedGuid' data-historyid='$row1_in1->itemGuid' data-status='$row1_in1->relateAs'>Delete</a><br>";
-    			}
+    			
     			
     		}
     		
