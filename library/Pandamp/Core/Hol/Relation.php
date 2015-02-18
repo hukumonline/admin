2e6d042fda4d6256aaa2e57553480c50c871ca0c
Nihki Prihadi <nihki@hukumonline.com>
2015-02-18 15:57:38 +0700

    modified getHistorynew

--- library/Pandamp/Core/Hol/Relation.php
+++ library/Pandamp/Core/Hol/Relation.php
@@ -62,7 +62,7 @@ class Pandamp_Core_Hol_Relation
 	    					$status = "[menetapkan]";
 	    				}
 	    				
-	    				$title = App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($row2_in1->itemGuid,'fixedTitle');
+	    				$title = App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($row2_in1->relatedGuid,'fixedTitle');
 	    				
     					$newh .= "<a href='".ROOT_URL.DS.'id'.DS.'dms/catalog/detail/guid/'.$row2_in1->itemGuid.'/node/'.$this->getNode($row2_in1->itemGuid)."'>$title</a> $status&nbsp<a href='javascript:;' class='historynew' data-guid='$row2_in1->relatedGuid' data-historyid='$row2_in1->itemGuid' data-status='$row2_in1->relateAs'>Delete</a><br>";
 	    				
