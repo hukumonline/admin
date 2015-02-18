2c46063ffb60775fd686955919c87f4b2932c626
Nihki Prihadi <nihki@hukumonline.com>
2015-02-18 16:35:24 +0700

    modified getHistorynew

--- library/Pandamp/Core/Hol/Relation.php
+++ library/Pandamp/Core/Hol/Relation.php
@@ -48,9 +48,9 @@ class Pandamp_Core_Hol_Relation
     		{
     			foreach ($row2 as $row2_in1) 
     			{
-//     				if ($row2_in1->itemGuid == "$guid") { 
-//     					continue;
-//     				} else { 
+    				if ($row2_in1->itemGuid == "$guid") { 
+    					continue;
+    				} else { 
     					
     					//$status = $this->getStatusHistory($row2_in1->itemGuid, $row2_in1->relatedGuid);
     					
@@ -72,7 +72,7 @@ class Pandamp_Core_Hol_Relation
     					$newh .= "<a href='".ROOT_URL.DS.'id'.DS.'dms/catalog/detail/guid/'.$row2_in1->relatedGuid.'/node/'.$this->getNode($row2_in1->relatedGuid)."'>$title</a> $status&nbsp<a href='javascript:;' class='historynew' data-guid='$row2_in1->relatedGuid' data-historyid='$row2_in1->itemGuid' data-status='$row2_in1->relateAs'>Delete</a><br>";
 	    				
 	    				//$newh .= $this->getchild($row2_in1->itemGuid);
-//     				}
+    				}
     			}
     			
     		}
