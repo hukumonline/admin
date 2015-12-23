86965c636e2e4038235da68abb6fe2353c137d53
Nihki Prihadi <nihki@hukumonline.com>
2015-12-23 16:31:43 +0700

    live request

--- app/modules/admin/controllers/LiveController.php
+++ app/modules/admin/controllers/LiveController.php
@@ -22,4 +22,32 @@ class Admin_LiveController extends Zend_Controller_Action
 		
 		echo number_format(App_Model_Mongodb_RequestLog::all()->count(true));
 	}
+	
+	public function timelineAction()
+	{
+		$this->_helper->layout()->disableLayout();
+		
+		$requestLog = App_Model_Mongodb_RequestLog::all()->sort(['_id' => -1])->limit(10);
+		
+		$content = 0;
+		$data = array();
+		foreach ($requestLog as $reqlog) {
+			$data[$content]['ip'] = $reqlog->ip;
+			$data[$content]['access_time'] = $reqlog->access_time;
+			
+			$content++;
+		}
+		
+		
+		$this->view->assign('data',$data);
+		$this->view->assign('dateFormat', array(
+				'DAY' 			=> '%s days ago',
+				'DAY_HOUR'		=> '%s days %s hours ago',
+				'HOUR' 			=> '%s hours ago',
+				'HOUR_MINUTE' 	=> '%s hours %s minutes ago',
+				'MINUTE' 		=> '%s minutes ago',
+				'MINUTE_SECOND'	=> '%s minutes %s seconds ago',
+				'SECOND'		=> '%s seconds ago',
+		));
+	}
 }
