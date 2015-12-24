cd78b32d1213049f2ad21d433fe23a0d81dc7bd8
Nihki Prihadi <nihki@hukumonline.com>
2015-12-24 22:51:06 +0700

    add user agent

--- library/Pandamp/Controller/Action/Helper/IsMobile.php
+++ library/Pandamp/Controller/Action/Helper/IsMobile.php
@@ -3,6 +3,6 @@ class Pandamp_Controller_Action_Helper_IsMobile
 {
 	public function isMobile($string)
 	{
-		return preg_match('/(iPhone|iPod|iPad|Android|BlackBerry)/i', $string);
+		return preg_match('/(iPhone|iPod|iPad|Android|BlackBerry|J2ME)/i', $string);
 	}
 }
