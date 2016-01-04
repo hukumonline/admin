6a8565e8f5aa15feac454fbb1b1e44ebe06ca23c
Nihki Prihadi <nihki@hukumonline.com>
2016-01-05 06:50:09 +0700

    modify percentage t9

--- app/models/Mongodb/RequestLog.php
+++ app/models/Mongodb/RequestLog.php
@@ -21,7 +21,6 @@ class App_Model_Mongodb_RequestLog extends Shanty_Mongo_Document
 		
 		$total = self::all($query)->count();
 		$pipeline = [
-			['$match' => $query],
 			[
 				'$group' => [
 					'_id' => '$full_url',
