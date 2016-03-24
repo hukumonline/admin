c15ea44ab81aab8c32d171aa57b346c95956ccdd
Nihki Prihadi <nihki@hukumonline.com>
2016-03-24 16:45:10 +0700

    try index pdf

--- app/controllers-cli/SolrController.php
+++ app/controllers-cli/SolrController.php
@@ -1510,7 +1510,7 @@ class SolrController extends Application_Controller_Cli
 						curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type:multipart/form-data'));
 						$result = curl_exec ($ch);*/
 						
-						$mapping_array = [
+						/*$mapping_array = [
 							"literal.id" => $guid,
 							"fmap.content" => "content",
 							"commit" => "true"
@@ -1529,7 +1529,9 @@ class SolrController extends Application_Controller_Cli
 							throw new Exception('Curl Error:' . curl_error($ch));
 							echo "<br/>Curl Error:<br/>" . curl_error($ch);
 						}
-						curl_close($ch);
+						curl_close($ch);*/
+						
+						shell_exec('curl "http://192.168.0.61:8983/solr/corehol/update/extract?literal.id="'.$guid.'"&fmap.content=content&commit=true" -F "myfile="'.@$sDir);
 						
 						/*$pdfExtractor = $this->_pdfExtractor;
 						system("$pdfExtractor ".$sDir.' '.$outpath, $ret);
