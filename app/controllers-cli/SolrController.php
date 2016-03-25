965df991470e64abee701049a9ad3e5dc198b348
Nihki Prihadi <nihki@hukumonline.com>
2016-03-25 22:36:10 +0700

    try index pdf file

--- app/controllers-cli/SolrController.php
+++ app/controllers-cli/SolrController.php
@@ -1521,10 +1521,9 @@ class SolrController extends Application_Controller_Cli
 						curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 						curl_setopt($ch, CURLOPT_URL, ($solr_extraction_endpoint . '?' . http_build_query($mapping_array,'','&')));
 						$cfile = curl_file_create($sDir);
-						
+						curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:multipart/form-data'));						
 						curl_setopt($ch, CURLOPT_POSTFIELDS, array('myfile' => $cfile));
 						
-						//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:multipart/form-data'));
 						//Execute curl.
 						if(!curl_exec($ch) == TRUE)
 						{
