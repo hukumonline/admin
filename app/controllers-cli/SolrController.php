e05a4e11b6b1e656d9c27686f3c6d8fe03619184
Nihki Prihadi <nihki@hukumonline.com>
2016-03-28 16:01:36 +0700

    try index pdf file

--- app/controllers-cli/SolrController.php
+++ app/controllers-cli/SolrController.php
@@ -1095,7 +1095,6 @@ class SolrController extends Application_Controller_Cli
 			}
 		}
 		}
-		Pandamp_Debug::manager($part);
 		return $part;
 	}
 	
@@ -1553,7 +1552,7 @@ class SolrController extends Application_Controller_Cli
 						$body = substr($result, $header_size);
 						curl_close($ch);*/
 						
-						system('curl "http://192.168.0.61:8983/solr/corehol/update/extract?literal.id="'.$guid.'"&fmap.content=content&commit=true" -F "myfile=@"'.$sDir);
+						system('curl "http://192.168.0.61:8983/solr/corehol/update/extract?literal.id="'.$guid.'"&fmap.content=content" -F "myfile=@"'.$sDir);
 						//system('curl "'.($solr_extraction_endpoint . '?' . http_build_query($mapping_array,'','&')).'" -F "myfile=@"'.$sDir);
 						
 						/*$pdfExtractor = $this->_pdfExtractor;
