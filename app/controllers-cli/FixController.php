948e7f063dca9801cac6ea50c941653f9ef62642
Nihki Prihadi <nihkiprihadi@yahoo.com>
2016-04-27 11:57:03 +0700

    add counter

--- app/controllers-cli/FixController.php
+++ app/controllers-cli/FixController.php
@@ -74,6 +74,70 @@ class FixController extends Application_Controller_Cli
 		echo "Fix st completed\n";
 	}
 	
+	public function counterAction()
+	{
+		$request = $this->getRequest();
+		$day = $request->getParam('q');
+		$db = $this->db;
+		$db->setFetchMode(Zend_Db::FETCH_OBJ);
+		$select = $db->select();
+		$select->from('KutuCatalog', '*');
+		$select->where("createdDate >= DATE_SUB(CURDATE(),INTERVAL $day DAY)"); // within the $day last
+		$rowsFound = $db->fetchAll($select);
+		
+		echo 'There are '.count($rowsFound)." catalog(s)\n";
+		
+		$rowCount = count($rowsFound);
+		for($iCount=0;$iCount<$rowCount;$iCount++) {
+			$row = $rowsFound[$iCount];
+				
+			if (isset($rowsFound[$iCount+1])) {
+				$nextRow = $rowsFound[$iCount+1];
+				$n = "-next:[".$nextRow->guid."]";
+			}
+			else
+				$n = '';
+		
+		
+			echo 'urutan: '.$iCount ." - ";
+		
+				
+			if ($row->profileGuid=='klinik')
+				$title = $this->getCatalogAttribute($row->guid, 'fixedCommentTitle');
+			else
+				$title = $this->getCatalogAttribute($row->guid, 'fixedTitle');
+				
+				
+			if ($title) {
+		
+				try {
+					
+					$desktop = $this->getCountCatalog($row->guid, $row->profileGuid, 'desktop');
+					$mobile = $this->getCountCatalog($row->guid, $row->profileGuid, 'mobile');
+		
+					//update document
+					/*$this->addHitsBySolr(json_encode([[
+							"id" => $row->guid,
+							"desktop" => ["set" => $desktop],
+							"mobile" => ["set" => $mobile]
+						]]));*/
+				}
+				catch (Zend_Exception $e)
+				{
+					throw new Zend_Exception($e->getMessage());
+				}
+			}
+				
+			echo "guid:[".$row->guid."]desktop:[".$desktop."]mobile:[".$mobile."][".$row->createdDate."]".$n."\n";
+		
+			flush();
+		}
+		
+		sleep(1);
+		
+		echo "Fix st completed\n";
+	}
+	
 	protected function addHitsBySolr($jsonData)
 	{
 		$indexing = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application-cli.ini','indexing');
@@ -98,5 +162,70 @@ class FixController extends Application_Controller_Cli
 		$sql->where('attributeGuid=?',$attributeGuid);
 		$row = $db->fetchRow($sql);
 		return ($row) ? $row->value : '';
+	}
+	
+	private function getCountCatalog($guid, $profileGuid, $type)
+	{
+		$valueText=null;
+		if (isset($profileGuid) && !in_array($profileGuid, array('partner','narsum','author','kategoriklinik','comment','about_us','kutu_contact','kutu_email','kutu_kotik','kutu_mitra','kutu_signup'))) {
+			switch ($type) {
+				case 'desktop':
+					if (in_array($profileGuid, array('article','talks','isuhangat','kutu_agenda'))) {
+						$valueText = 'TICKER';
+					}
+					else if ($profileGuid=='klinik') {
+						$valueText = 'klinik';
+					}
+					else if (in_array($profileGuid, array('kutu_peraturan','kutu_rancangan_peraturan','kutu_peraturan_kolonial','kutu_putusan')))
+					{
+						$valueText = 'pusatdata';
+					}
+	
+					break;
+	
+				case 'mobile':
+					if (in_array($profileGuid, array('article','talks','isuhangat','kutu_agenda'))) {
+						$valueText = 'TICKER-MOBILE';
+					}
+					else if ($profileGuid=='klinik') {
+						$valueText = 'klinik-mobile';
+					}
+					else if (in_array($profileGuid, array('kutu_peraturan','kutu_rancangan_peraturan','kutu_peraturan_kolonial','kutu_putusan')))
+					{
+						$valueText = 'pusatdata-mobile';
+					}
+	
+					break;
+			}
+	
+			$db = $this->db;
+			$db->setFetchMode(Zend_Db::FETCH_OBJ);
+				
+			$sql = $db->select();
+			$sql->from('KutuAssetSetting', ['valueInt']);
+			$sql->where('guid=?',$guid);
+	
+			if ($profileGuid=='kutu_doc') {
+				$sql->where('application=?','kutu_doc');
+			}
+			else
+			{
+				$sql->where('valueText=?',$valueText);
+			}
+	
+	
+	
+			$row = $db->fetchRow($sql);
+	
+			//$sql = $sql->__toString();
+			//print_r($sql);die;
+	
+			return ($row) ? $row->valueInt : 0;
+	
+	
+	
+		}
+	
+		return 0;
 	}
 }
